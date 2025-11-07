b3da534df6c204bfa5af3162d90117f9c60b1fbf
tsaebst <125909246+tsaebst@users.noreply.github.com>
2025-11-07 18:02:54 +0000

    macros upd

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,13 +1,16 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+
 mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
-    }
+    }//i'll let macro be usable out of module
+	pub(crate) use my_macro;
 }
 
 fn main() {
-    my_macro!();
+   // use macros::macro_rules;	
+   macros::my_macro!();
 }
